b79fd9d2176fe8a2a5772248a3c564f4efefb305
Denis Nebotov <elured@mail.ru>
2019-02-17 22:03:13 +0100

    WarenListe

---
+++ OnlineGameLaden.UnitTests/AdminArieaTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OnlineGameLaden.Domain.Abstract;
+using OnlineGameLaden.Domain.Entities;
+using OnlineGameLaden.WebUI.Controllers;
+using System.Linq;
+
+namespace OnlineGameLaden.UnitTests
+{
+    /// <summary>
+    /// Summary description for AdminArieaTests
+    /// </summary>
+    [TestClass]
+    public class AdminArieaTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Games()
+        {
+            // Организация - создание имитированного хранилища данных
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameId = 1, Name = "Игра1"},
+                new Game { GameId = 2, Name = "Игра2"},
+                new Game { GameId = 3, Name = "Игра3"},
+                new Game { GameId = 4, Name = "Игра4"},
+                new Game { GameId = 5, Name = "Игра5"}
+            });
+
+            // Организация - создание контроллера
+            AdminController controller = new AdminController(mock.Object);
+
+            // Действие
+            List<Game> result = ((IEnumerable<Game>)controller.Index().
+                ViewData.Model).ToList();
+
+            // Утверждение
+            Assert.AreEqual(result.Count(), 5);
+            Assert.AreEqual("Игра1", result[0].Name);
+            Assert.AreEqual("Игра2", result[1].Name);
+            Assert.AreEqual("Игра3", result[2].Name);
+        }
+    }
+}
---
+++ OnlineGameLaden.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using OnlineGameLaden.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineGameLaden.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IGameRepository repository;
+
+        public AdminController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Games);
+        }
+    }
+}
