baf68238b880b0e44bfb8a4c5fbfd0535e92805e
Denys <denys.nebotov@isaac.de>
2019-07-16 12:59:41 +0200

    Anzeigen die Bilder der Waren automatisch auf der Startseite

--- OnlineGameLaden.Domain/Concrete/EFDbContext.cs
+++ OnlineGameLaden.Domain/Concrete/EFDbContext.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace OnlineGameLaden.Domain.Concrete
 {
@@ -22,21 +25,21 @@ namespace OnlineGameLaden.Domain.Concrete
 
     public class EFDbInitializer : DropCreateDatabaseAlways<EFDbContext>//CreateDatabaseIfNotExists
     {
-
+        string pathToImagesOrder = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/");
         protected override void Seed(EFDbContext context)
         {
             List<Game> games = new List<Game>
             {
-                new Game{Name ="SimCity", Description = "1989 veröffentlichte EA einen Meilenstein der Spielegeschichte und Grundstein der Aufbau Simulation: SimCity. Knapp 20 Jahre später stellt der Entwickler nun die Städtebau", Category = "Simulation", Price= 15.00M},
-                new Game{Name ="TITANFALL", Description = "Im Vorfeld sind schon ein paar Infos über Titanfall geleakt. Es wird mit der Source-Engine laufen, die selbst nach neun Jahren noch unglaubliche Ergebnisse liefert, die PlayStation 4 wird vom Release ausgeschlossen und Titanfall wird nur einen Multiplayermodus beinhalten. Die Mitbegründer von „Infinity Ward“ liefern uns mit Titanfall einen sehr action-lastigen Multiplayer-Shooter für die Xbox One", Category = "Ego Shooter", Price= 25.00M},
-                new Game{Name ="Battlefield 4", Description = "Du bist bereit für die Schlacht? Dann ist die Battlefield 4 Premium Edition der perfekte Einstieg. Die Premium Edition gewährleistet, dass du alles hast, was du brauchst, um dich deinen Freunden anzuschließen und dich in First-Person-Schlachten für 64 Spieler zu stürzen.", Category = "Ego Shooter", Price= 10.49M},
-                new Game{Name ="The Sims 4", Description = "Die Sims steht seit jeher für eine echtzeit-strategische Lebenssimulation, in der ihr die Kontrolle über das Leben virtueller Figuren übernehmt. Dabei lässt sich sowohl das Arbeitsleben, als auch das soziale Leben der Spielfiguren komplett steuern und organisieren. Mit Die Sims 4 setzt EA konsequent die Serie um das künstliche Leben im PC fort.", Category = "Simulation", Price= 15.00M},
-                new Game{Name ="Dark Souls 2", Description = "Unter den zahlreichen mysteriösen Charakteren, die euch im Rollenspiel Dark Souls 2 begegnen, ist Ornifex eine der wichtigsten. Das Mischwesen halb Mensch, halb Krähe stellt einen wichtigen Händler dar, der euch zudem einige Bosswaffen schmieden kann. Wir verraten euch Standort und Funktion von Ornifex.", Category = "RPG", Price= 5.00M},
-                new Game{Name ="The Elder Scrolls V: Skyrim", Description = "Mit The Elder Scrolls V: Skyrim begibt sich Bethesda Softworks in die Richtung massentauglicher Rollenspiele. Mit dem fünften Teil um die Alten Schriftrollen Tamriels ist ihnen der Schritt gelungen, Fantasy-Rollenspiele wieder salonfähig zu machen und eines der besten Spiele dieser Tage zu kreieren.", Category = "RPG", Price= 14.99M},
-                new Game{Name ="FIFA 14", Description = "Nach Angaben von EA werden mehr als 3,4 Millionen Matches „Ultimate Team“ pro Tag gespielt und ist damit der populärste Spielmodus der FIFA-Reihe. Mit den neuen Features soll die Chemie zwischen den Spielern besser stimmen, die Spieltaktik individueller einstellbar sein und es wird für alle Matches mehr Rewards geben, mit denen man neue Spieler und Items freischalten kann. Außerdem kann man nun endlich seine eigenen, individuellen Trikos erstellen.", Category = "Simulation", Price= 6.00M},
-                new Game{Name ="Need for Speed Rivals", Description = "Kein Jahr ohne neues Need for Speed. Das Rennfranchise von Electronic Arts hat einmal mehr das Entwicklerstudio gewechselt. EA Ghosts aus Göteborg arbeitet jetzt an Need for Speed Rivals – und möchte mit ein paar Änderungen überzeugen.", Category = "Simulation", Price= 15.00M},
-                new Game{Name ="Crysis 3", Description = "Es musste ja kommen: Keine zwei Jahre nach dem Release von Crysis 2 legen Crytek und Electronic Arts mit Crysis 3 gleich nach. Was uns erwartet? Wohl wieder Action und Grafik vom Feinsten.", Category = "Ego Shooter", Price= 12.00M},
-                new Game{Name ="Dead Space 3", Description = "Rund acht Stunden sind vergangen und ich schaue auf die Uhr. Wie lang denn noch? Seit etwa sechs Stunden will ich bereits nicht mehr, war zunächst geschockt, dann sauer und will jetzt einfach nur noch, dass es endlich aufhört. Es ist einer dieser Momente im Alltag eines Spielejournalisten, wo das Hobby, das zum Beruf wurde, zur nervenden Arbeit ausartet. Dass es hier um eine Spielreihe geht, dessen Erstling ich verehrt habe, macht es kaum besser.", Category = "Ego Shooter", Price= 49.00M}
+                new Game{Name ="Original Rubik's Speedcube", ImageData = getImageAsByteArray(@"Images\ORIGINAL_RUBIKS_SPEED_CUBE.jpg"), ImageMimeType = "image/jpg", Description = "Endlich ein richtiger Speedcube von Original Rubik's! Fürs Speedcubing optimierter Mechanismus. Sehr smoothe Drehbewegungen. Robust und klassisch im Design. Die besten Köpfe von Rubik's und Gan (renommierter Speedcubing-Ausrüster seit 2014) haben sich zusammengetan, um diesen Meilenstein in der Cubing-Geschichte ins Leben zu rufen. Ambitionierte Speedcuber werden zustimmen, was der Weltklasse-Speedcuber Kevin Gerhardt zu diesem Produkt sagt:  \"Im Gegensatz zu anderen Rubik's Modellen kann der RSC mit den neuesten Speedcubes mithalten. Er vermittelt ein fantastisches Gefühl und lässt sich tadellos drehen. Er hat einen geschmeidigen Look und fühlt sich gut in der Hand an. Wenn du das Drehgefühl magst, hat er sogar das Potenzial dein neuer Main Cube zu werden!\"", Category = "Rubik's Cube", Price= 19.99M},//!!!
+                new Game{Name ="Speed Megaminx Cheeky Sheep", ImageData = getImageAsByteArray(@"Images\SPEED_MEGAMINX_CHEEKY_SHEEP.jpg"), ImageMimeType = "image/jpg", Description = "Beliebter Speed Megaminx für Anfänger und Fortgeschrittene. Fürs Speedcubing optimierte Bauweise. Mit Cornercutting. Der Speedcubing-Megaminx von Cubikon Cheeky Sheep in der neuen Ausführung (Modell 2019). Für Anfänger und für Fortgeschrittene geeignet. Erhöhte Bausteine (konvex) sorgen für besseren Grip, leichte Drehbarkeit, weniger Lockups (Verkanten). Ein optimaler Megaminx Cube für Einsteiger, die ein gutes Puzzle zum günstigen Preis suchen.", Category = "Rubik's Cube Variations", Price= 9.95M},//!!!
+                new Game{Name ="Speed Pyraminx Cheeky Sheep", ImageData = getImageAsByteArray(@"Images\SPEED_PYRAMINX_CHEEKY_SHEEP.jpg"), ImageMimeType = "image/jpg", Description = "Beliebter Speed Pyraminx für Anfänger und Fortgeschrittene. Fürs Speedcubing optimierte Bauweise. Mit Cornercutting. Leichte Drehbarkeit, Weniger Lockups (Verkanten), sehr gutes Cornercutting. Ein optimaler Pyraminx Cube für Einsteiger, die eine gute Dreh-Pyramide zum günstigen Preis suchen.", Category = "Rubik's Cube Variations", Price= 9.95M},//!!!
+                new Game{Name ="Mirror Cube Cheeky Sheep", ImageData = getImageAsByteArray(@"Images\MIRROR_CUBE_CHEEKY_SHEEP.jpg"), ImageMimeType = "image/jpg", Description = "Faszinierender Zauberwürfel, der nach Formen sortiert wird. Schöne neue Herausforderung für all diejenigen, denen der normale 3x3 Würfel zu langweilig ist oder einfach mal etwas Neues ausprobieren möchten.", Category = "Rubik's Cube", Price= 5.99M},//!!!
+                new Game{Name ="Original Rubik's Snake", ImageData = getImageAsByteArray(@"Images\original_rubiks_snake.jpg"), ImageMimeType = "image/jpg", Description = "Die Original Rubik's Snake von Jumbo Spiele ist eine verdrehte Denksport-Aufgabe, die Tausende von Formen annehmen kann. lange Zeit nicht erhältlich gewesen - jetzt wieder da von Jumbo Spiele! Farbplättchen sind schön eingearbeitet und nicht ablösbar Robuste und schöne Ausführung. Auch unter der Bezeichnung \"Rubik's Twist\" bekannt. Die neue Rubik's Snake ist ein netter Zeitvertreib für Zwischendurch.", Category = "Sonstige", Price= 14.95M},
+                new Game{Name ="Original Rubik's Touch Cube", ImageData = getImageAsByteArray(@"Images\original_rubiks_touch_cube.jpg"), ImageMimeType = "image/jpg", Description = "Fühlbare Muster auf den Farben - 3x3 Zauberwürfel zum Blindlösen! Besonderer Rubiks Cube - Harte Farblättchen mit erhabenen Details - zum Tasten. Der Klassiker in einer speziellen Auführung: Dies ist der Rubikwürfel für Blinde und zum Üben vom \"Blinden Lösen\" (sog. Blind Solves). Der Zauberwürfel ist das meistverkaufte Knobelspiel weltweit. Geniale Spielidee, der weitere faszinierende und sogar schwierigere Varianten folgten. Etliche davon finden Sie hier bei Cubikon im Angebot. 43 Trillionen(!) Kombinationen möglich. Dieser Würfel macht süchtig und trainiert die grauen Zellen. Ein Original von Rubik's. Neues, innovatives Design: Ermöglicht das Lösen mit verbundenen Augen(sog.Blind Solves). In schöner Geschenk - Verpackung. Inkl.Standfuß zur dekorativen Präsentation des Zauberwürfels. Inkl.Cubikon - Tasche zum Schutz gegen Schmutz und Staub", Category = "Sonstige", Price= 24.95M},
+                new Game{Name ="5 Stück Zauberwürfel in der Überraschungs-Kiste", ImageData = getImageAsByteArray(@"Images\5_stück_zauberwürfel_in_der_überraschungs-kiste.jpg"), ImageMimeType = "image/jpg", Description = "Jede Kiste ist eine Überraschung und enthält 5 verschiedene Spiele. Lass dich überraschen! Der gesamte Warenwert übersteigt den Kaufpreis erheblich. Fotos beispielhaft - z.B. Pyraminx, Megaminx, 2x2, 3x3, 4x4 Cube... Jede Kiste ist eine Überraschung und enthält 5 verschiedene Spiele. WICHTIGE INFORMATION: pro Box sind alle Artikel unterschiedlich -bei Bestellung mehrerer Boxen können doppelte Produkte enthalten sein. Ideal um verschiedene Zauberwürfel-Varianten kennen zu lernen. Thema ZAUBERWÜRFEL - alle Spiele sind Drehpuzzles und ähnlich zu lösen wie der bekannte Zauberwürfel", Category = "Sonstige", Price= 14.99M},
+                new Game{Name ="Speed Cube Gan356 Air Master", ImageData = getImageAsByteArray(@"Images\SPEED_CUBE_GAN356_AIR_MASTER.jpg"), ImageMimeType = "image/jpg", Description = "Ausgereifter Speedcube von Gans-Puzzle. Fürs Speedcubing optimierter Mechanismus. Sehr smoothe Drehbewegungen. Der GAN356AIR MASTER verfügt über ein ausgeklügeltes System, das ein sehr smoothes Drehgefühl vermittelt. Dieser Würfel ist ein Profi-Modell für ambitionierte Speedcuber. Im Lieferumfang enthalten sind austauschbare Federn um den Würfel noch individueller zu optimieren. Sehr leichtgängiger Würfel mit kaum Drehwiderstand austauschbare Federn (im Lieferumfang) für individuelle Optimierung, Starkes Cornercutting, Popresistent, Würfel der Profi-Klasse, robuste Sticker, teilweise Neon-Farben, Einstellwerkzeug enthalten", Category = "Rubik's Cube", Price= 34.95M},
+                new Game{Name ="Flower Cube Lucky Lion", ImageData = getImageAsByteArray(@"Images\FLOWER_CUBE_LUCKY_LION.jpg"), ImageMimeType = "image/jpg", Description = "Sehr beliebte Zauberwürfel-Variante. Solide Konstruktion und gute Drehbarkeit. Für Fortgeschrittene. Variation des Helikopter Cubes (Art. 9232), Ausgeklügelter, faszinierender Mechanismus, Geschwungene Linienführung, sehr gute Verarbeitung, flüssige Drehbewegungen sind möglich, eine Herausforderung, besonders geeignet für  fortgeschrittene Spieler, schönes Design, begehrt bei Zauberwürfel-Sammlern", Category = "Rubik's Cube Variations", Price= 12.90M},
+                new Game{Name ="Meffert's Gear Ball", ImageData = getImageAsByteArray(@"Images\MEFFERTS_GEAR BALL.jpg"), ImageMimeType = "image/jpg", Description = "Ball-Zauberwürfel mit Zahnrad Mechanismus. Robuste Fertigung ohne Sticker (Farbige Einzelteile). Qualität und Stabilität von Original Meffert's. Original Gear Ball von Meffert, steckt voller Herausforderungen, die einzelnen Cube-Teile drehen sich dank Zahnradtechnik beim Spielen um sich selbst, steigert die Konzentrationsfähigkeit und trainiert nebenbei den Grips", Category = "Rubik's Cube Variations", Price= 17.95M}
                 //new Game{Name ="", Description = "", Category = "", Price= 1M},
             };
 
@@ -47,6 +50,36 @@ namespace OnlineGameLaden.Domain.Concrete
             context.SaveChanges();
             base.Seed(context);
         }
+
+        private byte[] getImageAsByteArray(string path)
+        {
+            byte[] ret = null;
+            if (string.IsNullOrEmpty(path))
+                return ret;
+
+            //            string rootPath = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/" + path);//System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath;
+            path = Path.Combine(pathToImagesOrder, path);
+            if (!File.Exists(path))
+                return ret;
+
+            Image img = System.Drawing.Image.FromFile(path);
+            try
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    ret = ms.ToArray();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error by converting from image to byte: {e.Message}");
+            }
+            //ret = System.IO.File.ReadAllBytes(Server)
+            return ret;
+        }
+        
+
     }
 
 }
